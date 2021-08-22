c44d051d0c647ac56c5cac03f69393b124f5f1e3
Ayeven <dihi.mel@gmail.com>
2021-08-22 15:16:57 +0800

    update `interactionCreate` event

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -13,44 +13,44 @@ module.exports = {
 		const time = moment().format('YYYY MM DD HH:mm:ss');
 
 		if (interaction.isCommand()) {
-			const commandName = interaction.commandName;
-			const appCommand = appcommands.get(commandName);
-			if (!interaction.member) {
-				return interaction.reply({ content: 'Nope! can\'t do that in DM', ephemeral: true });
-			}
-
-			if(appCommand.owner && interaction.user.id !== owner) {
-				return interaction.reply({ content:'Nope! Only owner command', ephemeral: true });
-			}
-			// Check if there is any cooldown and set the cooldown if none exist
-			{
-				if (!cooldown.has(appCommand.name)) {
-					cooldown.set(appCommand.name, new Collection());
-				}
-			}
-
-			const now = Date.now();
-			const timestamps = cooldown.get(appCommand.name);
-			const cooldownAmount = (appCommand?.cooldown || 3) * 1000;
-
-			if (timestamps.has(interaction.user.id)) {
-				const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-				if (now < expirationTime) {
-					const timeLeft = (expirationTime - now) / 1000;
-					return interaction.reply({
-						content:`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${
-							appCommand.name
-						}\` command.`,
-						ephemeral: true },
-					);
-				}
-			}
-
-			timestamps.set(interaction.user.id, now);
-			setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
 			try {
+				const commandName = interaction.commandName;
+				const appCommand = appcommands.get(commandName);
+				if (!interaction.member) {
+					return interaction.reply({ content: 'Nope! can\'t do that in DM', ephemeral: true });
+				}
+
+				if(appCommand.owner && interaction.user.id !== owner) {
+					return interaction.reply({ content:'Nope! Only owner command', ephemeral: true });
+				}
+				// Check if there is any cooldown and set the cooldown if none exist
+				{
+					if (!cooldown.has(appCommand.name)) {
+						cooldown.set(appCommand.name, new Collection());
+					}
+				}
+
+				const now = Date.now();
+				const timestamps = cooldown.get(appCommand.name);
+				const cooldownAmount = (appCommand?.cooldown || 3) * 1000;
+
+				if (timestamps.has(interaction.user.id)) {
+					const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+					if (now < expirationTime) {
+						const timeLeft = (expirationTime - now) / 1000;
+						return interaction.reply({
+							content:`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${
+								appCommand.name
+							}\` command.`,
+							ephemeral: true },
+						);
+					}
+				}
+
+				timestamps.set(interaction.user.id, now);
+				setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
 				appCommand.slashcommand(interaction, appcommands);
 			}
 			catch (err) {
@@ -63,17 +63,19 @@ module.exports = {
 				const commandName = interaction.commandName;
 				const contextMenu = appcommands.get(commandName);
 				contextMenu.contextmenu(interaction, appcommands);
+				console.log(`[contextmenu] ${commandName} have been used! at ${time}`);
 			}
 			catch (error) {
-				console.warn(`${time} [contextmenu] ${error}`);
+				console.warn(`${time} [ContextMenu] ${error}`);
 			}
 		}
 
 		if (interaction.isButton()) {
 			try {
-				const CustomId = interaction.customId;
-				const slashCommand = appcommands.get(CustomId.split('_')[0]);
+				const customId = interaction.customId;
+				const slashCommand = appcommands.get(customId.split('_')[0]);
 				slashCommand.button(interaction, appcommands);
+				console.log(`[Button] ${customId} have been used! at ${time}`);
 			}
 			catch (err) {
 				console.warn(`${time} [Button] ${err}`);
@@ -82,9 +84,10 @@ module.exports = {
 
 		if (interaction.isSelectMenu()) {
 			try {
-				const CustomId = interaction.customId;
-				const slashCommand = appcommands.get(CustomId.split('_')[0]);
+				const customId = interaction.customId;
+				const slashCommand = appcommands.get(customId.split('_')[0]);
 				slashCommand.selectmenu(interaction, appcommands);
+				console.log(`[SelectMenu] ${customId} have been used! at ${time}`);
 			}
 			catch (err) {
 				console.warn(`${time} [SelectMenu] ${err}`);
